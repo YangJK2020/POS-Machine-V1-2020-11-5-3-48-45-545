25821353f52f5a81f3f67b8daed6489e77a4442b
Jiankun YANG <JYang53@slb.com>
2020-11-17 22:08:35 +0800

    Submit tasking diagram

--- src/PrintReceipt.ts
+++ src/PrintReceipt.ts
@@ -1,6 +1,7 @@
 import {loadAllItems, loadPromotions} from './Dependencies'
 
 export function printReceipt(tags: string[]): string {
+
   return `***<store earning no money>Receipt ***
 Name：Sprite，Quantity：5 bottles，Unit：3.00(yuan)，Subtotal：12.00(yuan)
 Name：Litchi，Quantity：2.5 pounds，Unit：15.00(yuan)，Subtotal：37.50(yuan)
@@ -10,3 +11,8 @@ Total：58.50(yuan)
 Discounted prices：7.50(yuan)
 **********************`
 }
+// function processInput(input:string[]): any[] {
+//   let results = [];
+  
+//   })
+//}
